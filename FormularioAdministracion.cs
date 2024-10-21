6e84089d0e63a88f54427c86cf00c24d4043560a
Oscar <ocariaga2024@alu.uct.cl>
2024-10-21 10:51:03 -0300

    Add update - admin mode

--- FormularioAdministracion.cs
+++ FormularioAdministracion.cs
@@ -13,6 +13,7 @@ namespace LoginWindows
 {
     public partial class FormularioAdministracion : Form
     {
+        int idSeleccionada = 0;
         public FormularioAdministracion()
         {
             InitializeComponent();
@@ -27,19 +28,63 @@ namespace LoginWindows
                 var command = connection.CreateCommand();
                 command.CommandText = "SELECT * FROM Usuarios";
 
+                // "adapter" lo extraí de chatgpt. no sabía que existía este tipo de clase!
                 using (var adapter = new SQLiteDataAdapter(command))
                 {
                     DataTable dataTable = new DataTable();
                     adapter.Fill(dataTable);
-                    dataGridView1.DataSource = dataTable; // Set the DataSource of DataGridView
+                    dataGridView1.DataSource = dataTable;
                 }
             }
         }
 
         private void editarBtn_Click(object sender, EventArgs e)
         {
+            using (var connection = new SQLiteConnection("Data Source=usuariosdb.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "UPDATE Usuarios SET nombre = @nombre, contraseña = @contraseña, ciudad = @ciudad, telefono = @telefono WHERE id = @id";
+                command.Parameters.AddWithValue("@nombre", usuarioInput.Text);
+                command.Parameters.AddWithValue("@contraseña", contraseñaInput.Text);
+                command.Parameters.AddWithValue("@ciudad", ciudadInput.Text);
+                command.Parameters.AddWithValue("@telefono", telefonoInput.Text);
+                command.Parameters.AddWithValue("@id", idSeleccionada);
+                command.ExecuteNonQuery();
 
+                command.CommandText = "SELECT * FROM Usuarios";
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
         }
 
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idSeleccionada = e.RowIndex + 1;
+
+            using (SQLiteConnection baseDatos = new SQLiteConnection("Data source=usuariosdb.db"))
+            {
+                baseDatos.Open();
+
+                var command = baseDatos.CreateCommand();
+                command.CommandText = @"SELECT * FROM Usuarios WHERE id = @idIngreso";
+                command.Parameters.AddWithValue("@idIngreso", idSeleccionada);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        usuarioInput.Text = reader.GetString(1);
+                        contraseñaInput.Text = reader.GetString(2);
+                        ciudadInput.Text = reader.GetString(3);
+                        telefonoInput.Text = reader.GetString(4);
+                    }
+                }
+            }
+        }
     }
 }
